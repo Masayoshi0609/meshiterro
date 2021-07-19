f9302d8718974fa002e90b08f98af0c03f7c39e8
Masayoshi0609 <masayoshi9876@gmail.com>
2021-07-19 09:45:21 +0000

    [Finish]6-9

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd1e5d6cce26e3868013753bc8b1a12e62f995076c76234747732c90edc6009c48d830dace77dd5f5aa6b2bcb8d23c71fd2a9f9e26d0fb12a9290490bb07e5e70'
